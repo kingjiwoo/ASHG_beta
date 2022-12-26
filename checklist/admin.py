de1f288637d286096db66f7ca2f6d3adb42cdc5e
Jiwoo Kim <jaydenkim2931@gmail.com>
2022-12-27 02:12:09 +0900

    도커 DB설정 추가

--- checklist/admin.py
+++ checklist/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from .models import checklist, features
 
 # Register your models here.
+# class ChecklistAdmin(admin.ModelAdmin):
+#     list_display =
+#     list_display_links = 
+#     list_filter = 
+
+
 class FeatureAdmin(admin.ModelAdmin):
-    
-
-
     list_display = ( 'question_id','division','place','object','question',)
     list_display_links = ('question_id',)
     list_filter = ('division', 'place','object','question',)
